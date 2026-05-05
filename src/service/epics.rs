c71d563956faea2e9e0c276ea132ca232afa5bda
Ragazoor <ragazoor@gmail.com>
2026-05-05 19:42:02 +0200

    fix(service): make has_any_field private in UpdateEpicParams

--- src/service/epics.rs
+++ src/service/epics.rs
@@ -24,7 +24,7 @@ pub struct UpdateEpicParams {
 }
 
 impl UpdateEpicParams {
-    pub(super) fn has_any_field(&self) -> bool {
+    fn has_any_field(&self) -> bool {
         !self.updated_field_names().is_empty()
     }
 
